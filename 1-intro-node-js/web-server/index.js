5adfe0d12e0a8f87c4133fd6c203f25a66263723
inncol-aestrada <aestrada@inncol.com.mx>
2025-07-22 08:25:35 -0600

    feat(overview): implement the template about the overview

--- 1-intro-node-js/web-server/index.js
+++ 1-intro-node-js/web-server/index.js
@@ -6,15 +6,43 @@ const fs = require('fs');
 // Lee el archivo JSON correctamente
 const data = fs.readFileSync(`${__dirname}/dev-data/fruits.json`, 'utf-8');
 
+const replaceTemplate = (template, element) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, element.productName);
+  output = output.replace(/{%IMAGE%}/g, element.image);
+  output = output.replace(/{%FROM%}/g, element.from);
+  output = output.replace(/{%PRICE%}/g, element.price);
+  output = output.replace(/{%NUTRIENTS%}/g, ...element.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, element.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, element.description);
+  output = output.replace(/{%ID%}/g, element.ID);
+
+  if (!element.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  }
+
+  return output
+}
+
+// read templates
+const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+
 const productsObject = JSON.parse(data);
 
 // Tabla de rutas (lookup table)
 const pathName = {
   '/overview': (res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Page about overview');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    const cardsHtml = productsObject.map((element) => replaceTemplate(templateCard, element)).join('') // .join('') convierte un array de strings en un solo string concatenado, sin separadores.
+
+    const ouput = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+    res.end(ouput);
   },
-  '/product?id': (res) => {
+  '/product': (res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Im Juan');
   },
@@ -27,7 +55,7 @@ const pathName = {
         'Content-Type': 'text/html',
         'my-own-header': 'Hello baby, can you see me ñ.ñr?'
      });
-    res.end('<h1> Hi main page <h1>');
+    res.end(templateOverview);
   },
 };
 
