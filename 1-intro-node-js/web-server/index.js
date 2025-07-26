0ee83af3ef82f105690f23513725a420b074ca6e
inncol-aestrada <aestrada@inncol.com.mx>
2025-07-26 12:03:23 -0600

    feat(modules): implementing modules in my api

--- 1-intro-node-js/web-server/index.js
+++ 1-intro-node-js/web-server/index.js
@@ -2,26 +2,10 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const { replaceTemplate } = require('./modules/renderFunction');
 
 // Lee el archivo JSON correctamente
 const data = fs.readFileSync(`${__dirname}/dev-data/fruits.json`, 'utf-8');
-
-const replaceTemplate = (template, element) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, element.productName);
-  output = output.replace(/{%IMAGE%}/g, element.image);
-  output = output.replace(/{%FROM%}/g, element.from);
-  output = output.replace(/{%PRICE%}/g, element.price);
-  output = output.replace(/{%NUTRIENTS%}/g, element.nutrients.join(' '));
-  output = output.replace(/{%QUANTITY%}/g, element.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, element.description);
-  output = output.replace(/{%ID%}/g, element.ID);
-
-  if (!element.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  }
-
-  return output;
-}
 
 // read templates
 const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
