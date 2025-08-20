60caceeb02c5997898c61e30883f65fe70e2a509
antcodernez <marshday5@gmail.com>
2025-08-19 22:50:25 -0600

    feat(env): implementing env variables and notes about path module

--- 3-asynchronous-js/index.js
+++ 3-asynchronous-js/index.js
@@ -1,33 +1,35 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 
-fs.readFile(`${__dirname}/cat.txt`, (err, data) => {
-    console.log(`Cat bread id: ${data}`);
+const { config } = require("../1-intro-node-js/config/");
 
-    fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${data}`, {
-        headers: {
-            "x-api-key": ""
+
+(async () => {
+  try {
+    // leer el archivo con await (promesa)
+    const data = await fs.readFile(`${__dirname}/cat.txt`, "utf-8");
+    console.log(`Cat breed id: ${data}`);
+    console.log(config);
+    
+    // fetch devuelve promesa
+    const res = await fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${data}`, {
+      headers: { 
+            "x-api-key": "live_uNoODOh2SVfDIrGd19uOclwkvWugLph8TiWcCAE7al1ddHf8NQUhU3t2UZUCeN6J" 
         }
-    })
-    .then((res) => {
-        return res.json()
-    })
-    .then(data => {
-        console.log(...data);
-
-        if (data === undefined || data === null) {
-            return console.log("Breead of a cat not found");
-        }
-        
-        const imageUrl = data[0].url;
-
-        fs.writeFile("cat-image.txt", imageUrl, (error) => {
-            if (error) {
-                return console.error("Error al guardar:", error);
-            }
-            console.log("URL de la imagen guardada en cat-image.txt");
-        });
-    })
-    .catch(err => {
-        console.error("Error en fetch:", err);
     });
-})
+
+    const json = await res.json();
+    console.log(...json);
+
+    if (!json || json.length === 0) {
+      return console.log("Breed of cat not found");
+    }
+
+    // escribir archivo con promesa
+    const imageUrl = json[0].url;
+    await fs.writeFile("cat-image.txt", imageUrl);
+
+    console.log("URL de la imagen guardada en cat-image.txt");
+  } catch (err) {
+    console.error("Error:", err);
+  }
+})();
